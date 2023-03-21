bb7ae905aedd73154e2cc272fe04cbe214f32d25
Your Name <JulienneJulien15@gmail.com>
2023-03-21 18:42:06 -0400

    Updated server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+// Port set up using the express app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// TO INFORM THE SERVER IS READY AND WHICH PORT BEING USED
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log('App listening on port ${PORT}!`');
+  });
+});
