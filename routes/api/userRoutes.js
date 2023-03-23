83e409e7b281273d27c676b583147227e4dc4880
Your Name <JulienneJulien15@gmail.com>
2023-03-23 17:15:09 -0400

    added routes to get users and posts

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+}= require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
