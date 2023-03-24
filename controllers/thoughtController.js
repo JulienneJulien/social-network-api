03971ec88ceacb92adae3e7c7b31bcc50e723f1b
Your Name <JulienneJulien15@gmail.com>
2023-03-24 18:51:11 -0400

    Added thoughtcontroller requirement -delete an existing reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -94,6 +94,20 @@ updateThought(req, res) {
         )
         .catch((err) => res.status(500).json(err));
     },         
-
-
-        };
+// ADDED REQUIREMENT - delete an existing reaction
+    deleteOneReaction(req, res) {
+        Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+            ? res
+                .status(404)
+                .json({ message: `No thought found with that ID` })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+};
